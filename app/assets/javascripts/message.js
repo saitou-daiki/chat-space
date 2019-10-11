5f4320110fe51fb09667fc06979efb649f5bf84e
saitou-daiki <alsocorykk@gmail.com>
2019-10-11 21:19:26 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -39,7 +39,7 @@ $(function(){
     .done(function(message){
       var html = buildMessage(message);
       $('.messages').append(html);
-      $('#message_content').val('')
+      $('form')[0].reset();
       $('.messages').animate({
         scrollTop: $('.messages')[0].scrollHeight});
      })
