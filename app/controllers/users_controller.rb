82dc188f7908601d7972628e6871919e314d3f23
saitou-daiki <alsocorykk@gmail.com>
2019-10-14 15:52:16 +0900

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+  def index
+    # @users = User.where(‘name LIKE(?) and id != ?’, “%#{params[:keyword]}%” ,current_user)
+    @users = User.where('name LIKE(?)',"%#{params[:keyword]}%").where.not(id: current_user.id)
+    render json: @users
+  end
   def edit
   end
-  def index
-    @users = User.include(:group)
-  end
-
   def update
     if current_user.update(user_params)
       redirect_to root_path
@@ -12,10 +13,8 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-
   private
-
   def user_params
     params.require(:user).permit(:name, :email)
   end
-end
+ end
