4f83605646d16a00ef6eb695c061acee1d40a498
saitou-daiki <alsocorykk@gmail.com>
2019-10-16 17:22:22 +0900

    テスト

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,17 +1,25 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
+<<<<<<< Updated upstream
   def index
 
   end
+=======
+>>>>>>> Stashed changes
 
   def new
     @group = Group.new
     @group.users << current_user
+    @members = @group.users
   end
 
   def create
+
     @group = Group.new(group_params)
+<<<<<<< Updated upstream
    
+=======
+>>>>>>> Stashed changes
 
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
@@ -34,10 +42,19 @@ class GroupsController < ApplicationController
 
   private
   def group_params
+<<<<<<< Updated upstream
     params.require(:group).permit(:name,{:user_ids => []})
+=======
+    params.require(:group).permit(:name, { :user_ids => [] })
+>>>>>>> Stashed changes
   end
 
   def set_group
     @group = Group.find(params[:id])
   end
+<<<<<<< Updated upstream
  end
+=======
+
+end
+>>>>>>> Stashed changes
